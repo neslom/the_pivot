1ed25052e2266e40f5ff31bf291debb81de5d69d
Valentino Espinoza <xvalentino13@gmail.com>
2015-04-01 10:32:56 -0600

    Added Lenders into the seed file
    creates 10 users without display name (nil)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,20 @@
+class Seed
+  def call
+    generate_lenders
+  end
 
+  def generate_lenders
+    @amount_of_lenders = 10
+
+    @amount_of_lenders.times do
+      user = User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 'password', role: 0)
+      puts "User Created: #{user.name}"
+    end
+  end
+
+  def self.call
+    new.call
+  end
+end
+
+Seed.call
