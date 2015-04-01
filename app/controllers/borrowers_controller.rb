f0c6822484c3f51d643da86017ac4e23e0ce4514
Markus Olsen <molsen13@gmail.com>
2015-04-01 15:07:20 -0600

    User can sign up as a borrower and be directed to borrower page

---
+++ app/controllers/borrowers_controller.rb
@@ -0,0 +1,27 @@
+class BorrowersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(borrower_params.merge(role: 1))
+    if user.save
+      session[:user_id] = user.id
+      flash[:notice] = "You've been saved"
+      redirect_to borrower_path(user)
+    else
+      flash[:error] = "Something went wrong. Please try again"
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+  def borrower_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end
--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -34,4 +34,29 @@ RSpec.describe "User Signup" do
     end
 
   end
+
+  context "as a borrower" do
+
+    it "can sign up with valid information" do
+      expect(User.count).to eq(0)
+
+      visit '/'
+      click_link_or_button("Sign Up")
+      click_link_or_button("As Borrower")
+
+      fill_in("user[name]", with: "Sally")
+      fill_in("user[email]", with: "sally@example.com")
+      fill_in("user[password]", with: "password")
+      fill_in("user[password_confirmation]", with: "password")
+      click_link_or_button("Create my account")
+
+      expect(User.count).to eq(1)
+
+      user = User.first
+      expect(user.name).to eq("Sally")
+      expect(user.role).to eq("borrower")
+      expect(current_path).to eq(borrower_path(user))
+    end
+
+  end
 end
