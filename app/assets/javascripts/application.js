c434a69d4182fba691ff9fb306303c7b36ddc275
Markus Olsen <molsen13@gmail.com>
2015-04-02 11:24:22 -0600

    Moving order of app.js requirements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
-//= require jquery.turbolinks
 //= require_tree .
