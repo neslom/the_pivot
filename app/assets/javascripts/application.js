63f05b34c2c9ce90eea293e5b3df83260d92e0e2
Markus Olsen <molsen13@gmail.com>
2015-04-02 11:13:42 -0600

    Change order of jquery.turbolinks is application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require twitter/bootstrap
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
-//= require jquery.turbolinks
 //= require_tree .
