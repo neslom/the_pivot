8e9c581177d8a15869dc62ecbc5fd00441c4a2ef
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-05 13:41:00 -0600

    feature spec for user log in now works with proper behavior

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -1,89 +1,57 @@
 require "rails_helper"
 
 RSpec.describe "User logs in" do
+  let(:lender) { User.create(email: "example@example.com", password: "password", name: "richard", role: 0) }
+  let(:borrower) { User.create(email: "example@example.com", password: "password", name: "sally", role: 1) }
 
   context "with invalid log in credentials" do
 
     scenario "cannot log in without email or password" do
-      visit "/login"
-      click_link_or_button "Login"
+      visit "/"
+      click_link_or_button "Log In"
       within(".flash") do
         expect(page).to have_content("Invalid Login")
       end
     end
 
     scenario "cannot log in without email" do
-      visit "/login"
+      visit "/"
       fill_in("session[password]", with: "pass")
-      click_link_or_button "Login"
+      click_link_or_button "Log In"
       within(".flash") do
         expect(page).to have_content("Invalid Login")
       end
     end
 
     scenario "cannot log in without password" do
-      visit "/login"
+      visit "/"
       fill_in("session[email]", with: "example@example.com")
-      click_link_or_button "Login"
+      click_link_or_button "Log In"
       within(".flash") do
         expect(page).to have_content("Invalid Login")
       end
     end
+  end
 
-    scenario "logging in does not clear the cart" do
-      user = User.create(full_name: "dg",
-                         email: "example@example.com",
-                         password: "password",
-                         display_name: "example name")
-      # create(:item)
-      visit "/menu"
-      within("#items") do
-        click_link_or_button "Add To Cart"
-      end
-      visit "/cart"
-      expect(page).to have_content("Sushi")
-      expect(page).to_not have_content("Onigiri")
-      expect(page).to have_content("1")
-      visit "/login"
-      fill_in("session[email]", with: user.email)
-      fill_in("session[password]", with: user.password)
-      click_link_or_button "Login"
-      expect(page).to have_content("Welcome to the dojo")
-      visit "/cart"
-      expect(page).to have_content("Sushi")
-      expect(page).to_not have_content("Onigiri")
-      expect(page).to have_content("1")
+  context "as a valid lender" do
+
+    scenario "can log in" do
+      visit "/"
+      fill_in("session[email]", with: lender.email)
+      fill_in("session[password]", with: lender.password)
+      click_link_or_button "Log In"
+      expect(page).to have_content("#{lender.name}'s Portfolio")
     end
   end
 
-  context "as a valid default user" do
-    let(:user) { User.create(full_name: "dg",
-                             email: "example@example.com",
-                             password: "password",
-                             display_name: "example name") }
+  context "as a valid borrower" do
 
     scenario "can log in" do
-      visit "/login"
-      fill_in("session[email]", with: user.email)
-      fill_in("session[password]", with: user.password)
-      click_link_or_button "Login"
-      expect(page).to have_content("Welcome to the dojo")
-    end
-  end
-
-  context "as a valid admin" do
-    let(:admin) { User.create(full_name: "dg",
-                              email: "example@example.com",
-                              password: "password",
-                              display_name: "example name",
-                              role: 1) }
-
-    scenario "can log in" do
-      visit "/login"
-      fill_in("session[email]", with: admin.email)
-      fill_in("session[password]", with: admin.password)
-      click_link_or_button "Login"
-      expect(page).to have_content("Welcome to the admin dashboard")
+      visit "/"
+      fill_in("session[email]", with: borrower.email)
+      fill_in("session[password]", with: borrower.password)
+      click_link_or_button "Log In"
+      expect(page).to have_content("#{borrower.name}'s Projects")
     end
   end
 end
