4dff117e25c464e602baa1e89ea7450755991a4c
Markus Olsen <molsen13@gmail.com>
2015-04-05 15:35:43 -0600

    Remove duplication in rails helper, and logged in borrower cannot see another borrower's create or edit link

--- spec/features/borrower_authorization_spec.rb
+++ spec/features/borrower_authorization_spec.rb
@@ -1,18 +1,63 @@
 require 'rails_helper'
 
 RSpec.feature "only authorized borrower can create or edit loan requests" do
-  let!(:borrower) { User.create(name: "Richard",
+  let!(:borrower_1) { User.create(name: "Richard",
                                 email: "richard@example.com",
                                 password: "password",
                                 role: "borrower")
   }
 
+  let!(:borrower_2) { User.create(name: "Sally",
+                                email: "sally@example.com",
+                                password: "password",
+                                role: "borrower")
+  }
+
+  before(:each) do
+    borrower_1.loan_requests.create(title: "Farm Tools",
+                                  description: "help out with the farm tools",
+                                  amount: "$100.00",
+                                  requested_by_date: "2015-06-01",
+                                  repayment_begin_date: "2015-12-01",
+                                  repayment_rate: "Monthly")
+
+    borrower_2.loan_requests.create(title: "Laundry Machine",
+                                  description: "help out with the laundry",
+                                  amount: "$100.00",
+                                  requested_by_date: "2015-06-01",
+                                  repayment_begin_date: "2015-12-01",
+                                  repayment_rate: "Weekly")
+  end
+
   context "unauthenticated user" do
     scenario "visits borrower page and does not see create or edit links" do
-      visit borrower_path(borrower)
+      visit borrower_path(borrower_1)
 
       expect(page).to_not have_link("Create Loan Request")
       expect(page).to_not have_link("Edit")
+
+      loan_request = borrower_1.loan_requests.first
+      expect(page).to have_link("About", href: loan_request_path(loan_request))
     end
+  end
+
+  context "authenticated borrower" do
+    scenario "visits another borrower's page and does not see create or edit links" do
+      set_current_user(borrower_1)
+
+      visit borrower_path(borrower_2)
+      loan_request = borrower_2.loan_requests.first
+
+      expect(page).to_not have_link("Create Loan Request")
+      expect(page).to have_link("About", href: loan_request_path(loan_request))
+    end
+
+    xscenario "visits own page and sees create and edit links" do
+      set_current_user(borrower_1)
+      visit borrower_path(borrower_2)
+
+      expect(page).to_not have_link("Create Loan Request")
+    end
+
   end
 end
