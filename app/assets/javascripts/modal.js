ee21e2fb109be059a939de1bfc7f59bb0d9fc008
Markus Olsen <molsen13@gmail.com>
2015-04-06 22:00:51 -0600

    Rename 'hideModal' JS function

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,5 +1,6 @@
-var travis = function() {
+var hideModal = function() {
   $("#submit-button").click(function() {
-    $(this).parent().parent().parent().parent().parent().remove();
+    //$(this).parent().parent().parent().parent().parent().remove();
+    $(".modal-content").remove();
   });
 };
