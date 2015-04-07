f13a5e1773ad59eb339d501a837f09b551d57e65
Markus Olsen <molsen13@gmail.com>
2015-04-06 22:04:19 -0600

    removed multiple parent calls from hideModal() and deleted _item and edit from loan request views

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,6 +1,5 @@
 var hideModal = function() {
   $("#submit-button").click(function() {
-    //$(this).parent().parent().parent().parent().parent().remove();
     $(".modal-content").remove();
   });
 };
