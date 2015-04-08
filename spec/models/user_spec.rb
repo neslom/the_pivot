8cd9f8a287e0dcbb0beb2d84528fff5dac19d993
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-08 08:52:46 -0600

    hound fixes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,31 +3,31 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   let!(:user) { User.create(email: "example@example.com", password: "password", name: "example") }
 
-    it "is valid" do
-      expect(user).to be_valid
-    end
+  it "is valid" do
+    expect(user).to be_valid
+  end
 
-    it "is invalid without a password" do
-      user.password = nil
-      expect(user).to_not be_valid
-    end
+  it "is invalid without a password" do
+    user.password = nil
+    expect(user).to_not be_valid
+  end
 
-    it "is invalid without an email address" do
-      user.email = nil
-      expect(user).to_not be_valid
-    end
+  it "is invalid without an email address" do
+    user.email = nil
+    expect(user).to_not be_valid
+  end
 
-    it "is invalid without a name" do
-      user.name = nil
-      expect(user).to_not be_valid
-    end
+  it "is invalid without a name" do
+    user.name = nil
+    expect(user).to_not be_valid
+  end
 
-    it "is given a role of lender by default" do
-      expect(user.role).to eq("lender")
-    end
+  it "is given a role of lender by default" do
+    expect(user.role).to eq("lender")
+  end
 
-    it "is invalid with a duplicate email address" do
-      user2 = User.create(email: "example@example.com", password: "password", name: "example")
-      expect(user2).to_not be_valid
-    end
+  it "is invalid with a duplicate email address" do
+    user2 = User.create(email: "example@example.com", password: "password", name: "example")
+    expect(user2).to_not be_valid
+  end
 end
