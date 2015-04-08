14a0bbed52a259f61aa5b6e8481a23a23cf4356d
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-08 08:46:47 -0600

    modified user_spec setup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,32 +1,29 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-
-  before(:each) do
-    @user = User.create(email: "example@example.com", password: "password", name: "example")
-  end
+  let!(:user) { User.create(email: "example@example.com", password: "password", name: "example") }
 
     it "is valid" do
-      expect(@user).to be_valid
+      expect(user).to be_valid
     end
 
     it "is invalid without a password" do
-      @user.password = nil
-      expect(@user).to_not be_valid
+      user.password = nil
+      expect(user).to_not be_valid
     end
 
     it "is invalid without an email address" do
-      @user.email = nil
-      expect(@user).to_not be_valid
+      user.email = nil
+      expect(user).to_not be_valid
     end
 
     it "is invalid without a name" do
-      @user.name = nil
-      expect(@user).to_not be_valid
+      user.name = nil
+      expect(user).to_not be_valid
     end
 
     it "is given a role of lender by default" do
-      expect(@user.role).to eq("lender")
+      expect(user.role).to eq("lender")
     end
 
     it "is invalid with a duplicate email address" do
