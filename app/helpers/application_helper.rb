dc50d138130afad4f38b1cd40fde4bc775aacf07
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-09 21:23:07 -0600

    more hound fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
   end
 
-  def flash_messages(opts = {})
+  def flash_messages
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: "alert") do
         concat(content_tag(:button, class: "close", data: { dismiss: "alert" }) do
