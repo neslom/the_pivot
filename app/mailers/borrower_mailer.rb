c8281cfbbb3115f06e719b0a20dc303f50eec34a
Markus Olsen <molsen13@gmail.com>
2015-04-11 20:40:47 -0600

    Successfully testing that an email can be sent

--- app/mailers/borrower_mailer.rb
+++ app/mailers/borrower_mailer.rb
@@ -1,9 +1,9 @@
 class BorrowerMailer < ApplicationMailer
-  default from: "notifications@example.com"
+  default from: "notifications@keevahh.com"
 
   def project_contributed_to(user)
     @user = user
     @url = "http://protected-lowlands-2991.herokuapp.com"
-    mail(to: @user.email, subject: "Your project has been contributed to.")
+    mail(to: @user.email, subject: "Your project has been contributed to!")
   end
 end
--- spec/mailers/borrower_mailer_spec.rb
+++ spec/mailers/borrower_mailer_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe BorrowerMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { User.create(name: "Richard", email: "richard@example.com", password: "password") }
+
+  it "sends an email upon loan request contribution" do
+    mail = BorrowerMailer.project_contributed_to(user)
+    expect(mail.subject).to eq("Your project has been contributed to!")
+    expect(mail.to).to eq([user.email])
+    expect(mail.from).to eq(["notifications@keevahh.com"])
+    expect(mail.body.encoded).to include(user.name)
+    #expect(mail.body.encoded).to include(CGI::escape(user.email))
+  end
 end
