28470ac12aefabf59c97a57324512aee5c06b900
Markus Olsen <molsen13@gmail.com>
2015-04-12 13:14:20 -0600

    Edit the email views and write some basic tests to see that the emails are being rendered correctly

--- spec/mailers/borrower_mailer_spec.rb
+++ spec/mailers/borrower_mailer_spec.rb
@@ -5,10 +5,11 @@ RSpec.describe BorrowerMailer, type: :mailer do
   let(:user) { User.create(name: "Richard", email: "richard@example.com", password: "password") }
 
   it "sends an email upon loan request contribution" do
-    mail = BorrowerMailer.project_contributed_to(user)
+    mail = BorrowerMailer.project_contributed_to(user, "Project Title")
     expect(mail.subject).to eq("Your project has been contributed to!")
     expect(mail.to).to eq([user.email])
     expect(mail.from).to eq(["notifications@keevahh.com"])
     expect(mail.body.encoded).to include(user.name)
+    expect(mail.body.encoded).to include("Project Title")
   end
 end
