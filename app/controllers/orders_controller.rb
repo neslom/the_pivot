50ee5f7a975b42865d5f92e994551484c1887779
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-15 17:02:37 -0600

    removed unnecessary methods in orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,33 +12,4 @@ class OrdersController < ApplicationController
       redirect_to cart_path
     end
   end
-
-  def index
-    if current_user && current_user.user?
-      @orders = current_user.orders
-    elsif current_user.admin?
-      @orders = Order.all
-    else
-      flash[:notice] = "You Must Be Logged In"
-      redirect_to login_path
-    end
-  end
-
-  def show
-    @order = Order.find(params[:id])
-  end
-
-  def update
-    @order = Order.find(params[:id])
-    if params[:status] == "paid"
-      @order.paid!
-      redirect_to order_path
-    elsif params[:status] == "cancelled"
-      @order.cancelled!
-      redirect_to order_path
-    elsif params[:status] == "completed"
-      @order.completed!
-      redirect_to order_path
-    end
-  end
 end
