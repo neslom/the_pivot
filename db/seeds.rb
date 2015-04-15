568c9c4b8f941b48af71f6e9075cd3338fb9a69c
Valentino Espinoza <xvalentino13@gmail.com>
2015-04-15 15:26:02 -0600

    number of users and requests changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ class Seed
   def run
     create_known_users
     create_borrowers(10)
-    create_lenders(10)
-    create_loan_requests_for_each_borrower(3)
+    create_lenders(100)
+    create_loan_requests_for_each_borrower(20)
     create_categories
     create_orders
   end
