751ff4c700f6e284a0e387cbe9f23509b6417c58
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-15 13:08:10 -0600

    spec for over-contribution

--- spec/features/lender_contributes_to_loan_request_spec.rb
+++ spec/features/lender_contributes_to_loan_request_spec.rb
@@ -25,35 +25,54 @@ RSpec.feature "lender contributes to loan request" do
 
   let(:category) { Category.create(title: "agricultuer", description: "agri stuff") }
 
-  before(:each) do
-    loan_request.categories << category
-    visit "/"
-    login_as(lender)
-    visit browse_path
-    click_link_or_button("Contribute $25")
-    visit cart_path
-    click_link_or_button("Transfer Funds")
+  context "successfully transfers funds" do
+    before(:each) do
+      loan_request.categories << category
+      visit "/"
+      login_as(lender)
+      visit browse_path
+      click_link_or_button("Contribute $25")
+      visit cart_path
+      click_link_or_button("Transfer Funds")
+    end
+
+    scenario "transfers funds successfully" do
+      expect(current_path).to eq(lender_path(lender))
+      expect(page).to have_content("Thank you for your contribution, #{lender.name}!")
+
+      visit cart_path
+      expect(page).to_not have_content(loan_request.title)
+    end
+
+    scenario "after funds are transferred the contribution is reflected on the borrower show page" do
+      visit borrower_path(borrower)
+      click_link_or_button("About")
+
+      expect(page).to have_content("$75.00")
+    end
+
+    scenario "sees loan request contribution on portfolio page" do
+      expect(current_path).to eq(lender_path(lender))
+      [loan_request.title, loan_request.repayment_rate.capitalize, "$25.00"].each do |x|
+        expect(page).to have_content(x)
+      end
+    end
   end
 
-  scenario "transfers funds successfully" do
-    expect(current_path).to eq(lender_path(lender))
-    expect(page).to have_content("Thank you for your contribution, #{lender.name}!")
-
-    visit cart_path
-    expect(page).to_not have_content(loan_request.title)
-  end
-
-  scenario "after funds are transferred the contribution is reflected on the borrower show page" do
-    visit borrower_path(borrower)
-    click_link_or_button("About")
-
-    expect(page).to have_content("$75.00")
-  end
-
-  scenario "sees loan request contribution on portfolio page" do
-    expect(current_path).to eq(lender_path(lender))
-    [loan_request.title, loan_request.repayment_rate.capitalize, "$25.00"].each do |x|
-      expect(page).to have_content(x)
+  context "unsuccessfully transfers funds" do
+    scenario "cannot contribute more than what is needed" do
+      loan_request.categories << category
+      visit "/"
+      login_as(lender)
+      visit browse_path
+      click_link_or_button("Contribute $25")
+      visit cart_path
+      within(".add25") do
+        4.times { click_link_or_button "$25" }
+      end
+      expect(page).to have_content("$125.00")
+      click_link_or_button "Transfer Funds"
+      expect(page).to have_content("Your contribution could not be completed.")
     end
   end
 end
