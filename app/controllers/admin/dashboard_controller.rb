8b6fe63da5820baf91de02a45bacc2869f40e6f2
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-16 16:42:43 -0600

    more refactoring

--- app/controllers/admin/dashboard_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::DashboardController < AdminController
-  def index
-    if current_user.nil? || current_user.user?
-      redirect_to root_path
-    else
-      render :index
-    end
-  end
-end
