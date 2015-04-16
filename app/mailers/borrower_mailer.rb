6c60fd0556621e27273948b6707d123ed7555119
Markus Olsen <molsen13@gmail.com>
2015-04-16 16:02:46 -0600

    Change url to lendkeevahh

--- app/mailers/borrower_mailer.rb
+++ app/mailers/borrower_mailer.rb
@@ -4,7 +4,7 @@ class BorrowerMailer < ApplicationMailer
   def project_contributed_to(user, project_title)
     @user = user
     @project_title = project_title
-    @url = "http://protected-lowlands-2991.herokuapp.com"
+    @url = "http://lendkeevahh.herokuapp.com"
     mail(to: @user.email, subject: "Your project has been contributed to!")
   end
 end
