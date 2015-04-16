c7e3c57c62ac678dbc02f5e4dc832dab2c48cbed
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-15 19:03:00 -0600

    hound fixes

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,18 +2,17 @@ require "rails_helper"
 
 RSpec.describe Order, type: :model do
   context "valid attributes" do
-
-  let!(:loan_request) { LoanRequest.create(title: "Farm Tools",
-                                           description: "help out with the farm tools",
-                                           amount: "100",
-                                           requested_by_date: "2015-06-01",
-                                           repayment_begin_date: "2015-12-01",
-                                           contributed: "50",
-                                           repayment_rate: "monthly",
-                                           user_id: borrower.id)
-  }
+    let!(:loan_request) { LoanRequest.create(title: "Farm Tools",
+                                             description: "help out with the farm tools",
+                                             amount: "100",
+                                             requested_by_date: "2015-06-01",
+                                             repayment_begin_date: "2015-12-01",
+                                             contributed: "50",
+                                             repayment_rate: "monthly",
+                                             user_id: borrower.id)
+    }
     let(:borrower) { User.create(email: "example@example.com", password: "password", name: "sally", role: 1) }
-    let(:order) { Order.create(user_id: 1, cart_items: {"#{loan_request.id}" => 25})}
+    let(:order) { Order.create(user_id: 1, cart_items: { "#{loan_request.id }" => 25 }) }
 
     scenario "it is valid" do
       expect(order).to be_valid
